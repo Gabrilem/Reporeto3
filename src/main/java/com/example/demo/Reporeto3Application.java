6d63694ab977bf1b9f121b95632d67d5196f126c
Gabrilem <gabrielgaddiel@gmail.com>
2022-10-12 22:36:57 -0500

    first commit

---
+++ src/main/java/com/example/demo/Reporeto3Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reporeto3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reporeto3Application.class, args);
+	}
+
+}
